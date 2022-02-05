9bcc97540b2296803c11f0d082746bbcbae17869
WataruNishimura <wataru.nishimura@nnkk.work>
2022-02-06 02:21:29 +0900

    save token data to firebase realtime database

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import json
+import optparse
 from urllib import parse
 from modules.jwt import getJWTtoken
 from fastapi import FastAPI
 import requests
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
 import uvicorn
 
 messaging_api_url = "https://api.line.me/oauth2/v2.1"
@@ -20,5 +24,19 @@ if __name__ == "__main__":
     "client_assertion_type": "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
     "client_assertion": jwt 
   }
-  token_response = requests.post(messaging_api_url + "/token" , data=payload)
-  print(token_response.text)
+  response = requests.post(messaging_api_url + "/token" , data=payload)
+
+  token_dictionary = json.loads(response.content.decode())
+
+  print(token_dictionary["message"])
+
+  credential = credentials.Certificate("credentials.json")
+  firebase_admin.initialize_app(credential=credential, options={
+    "databaseURL": "https://darts-chatbot-example-default-rtdb.asia-southeast1.firebasedatabase.app/"
+  })
+
+  ref = db.reference("line")
+  ref.set({
+    #"token": token_dictionary.access_token,
+    #"key_id": token_dictionary.key_id
+  })
