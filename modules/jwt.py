bcfe95e7a7f8d00b30a779b099273f7acb2f8c8c
WataruNishimura <wataru.nishimura@nnkk.work>
2022-02-07 17:22:29 +0900

    change token_exp by env mode

--- modules/jwt.py
+++ modules/jwt.py
@@ -1,3 +1,4 @@
+from lib2to3.pgen2 import token
 import jwt
 from jwt.algorithms import RSAAlgorithm
 import time
@@ -5,19 +6,30 @@ import json
 from pathlib import Path
 import os
 
+mode = os.getenv("MODE") if os.getenv("MODE") else "development"
+
+token_exp = 60*60*24*7 # 60s 60min 24hour 7days
+
+if(mode == "development") :
+  token_exp = 60*60*24*7 # 60s 60min 24hour 1day
+elif( mode=="production" ):
+  token_exp = 60*60*24*7 # 60s 60min 24hour 30day
+
 def getJWTtoken(directory):
   filepath = Path(directory)
   private_key_file = open(filepath / "private_key.json", mode="rt", encoding="utf-8")
   header_file = open(filepath / "header.json", mode="rt", encoding="utf_8")
   private_key = json.load(private_key_file)
   header = json.load(header_file)
-  
+
+  print(token_exp)
+
   payload = {
     "iss": os.getenv("CLIENT_ID"),
     "sub": os.getenv("CLIENT_ID"),
     "aud": "https://api.line.me/",
     "exp": int(time.time()) + (60*30),
-    "token_exp": 60*60*24*30
+    "token_exp": token_exp
   }
 
   key = RSAAlgorithm.from_jwk(private_key)
