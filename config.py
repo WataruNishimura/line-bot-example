17029fbb240ca889a3c7f5012e0b9e8230ba7833
WataruNishimura <wataru.nishimura@nnkk.work>
2022-02-10 23:17:21 +0900

    add two env

--- config.py
+++ config.py
@@ -4,6 +4,8 @@ load_dotenv()
 import os
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+MODE = os.getenv("MODE")
+HOST = os.getenv("HOST")
 
 if __name__ == "__main__":
   print(CLIENT_ID, CLIENT_SECRET)
