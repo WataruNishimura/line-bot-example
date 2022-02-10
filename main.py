199ea4faa533fc6fa55018104c687a05f0b3e010
WataruNishimura <wataru.nishimura@nnkk.work>
2022-02-10 23:19:53 +0900

    prepare for webhook

--- main.py
+++ main.py
@@ -1,45 +1,54 @@
+from asyncio.log import logger
+from inspect import signature
 import json
 import os
-import config
+from traceback import print_tb
+import config 
 from modules.jwt import getJWTtoken
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response, Header
 import requests
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 import uvicorn
-from modules.api import getValidChannelAccessToken, isValidChannelAccessToken
+from modules.api import getValidChannelAccessToken, isValidChannelAccessToken, setWebhookEndpoint
+import base64
+import hashlib
+import hmac
+from pydantic import BaseModel
+import logging
 
 messaging_api_url = "https://api.line.me/oauth2/v2.1"
 
 mode = os.getenv("MODE") if os.getenv("MODE") else "development"
-
+channel_secret = os.getenv("CLIENT_SECRET")
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello, world"}
 
-if __name__ == "__main__":
-  jwt = getJWTtoken(".")
-  payload = {
-    "grant_type": "client_credentials",
-    "client_assertion_type": "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
-    "client_assertion": jwt 
-  }
+logger = logging.getLogger("uvicorn")
+class WebhookData(BaseModel):
+  destination: str
+  events: list
 
-  kids = getValidChannelAccessToken(jwt=jwt)
+@app.on_event("startup")
+async def startup():
 
+  access_token = ""
 
   credential = credentials.Certificate("credentials.json")
   firebase_admin.initialize_app(credential=credential, options={
     "databaseURL": "https://darts-chatbot-example-default-rtdb.asia-southeast1.firebasedatabase.app/"
   })
 
-  ref = db.reference("line")
+  jwt = getJWTtoken(".")
 
-  access_token = ""
+  payload = {
+    "grant_type": "client_credentials",
+    "client_assertion_type": "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
+    "client_assertion": jwt 
+  }
 
+  kids = getValidChannelAccessToken(jwt=jwt) #有効なトークンのkidが返ってくる
   if(len(kids) == 0) : #発行済みのトークンがなかった場合
     response = requests.post(messaging_api_url + "/token" , data=payload) #tokenを取得
     token_dictionary = json.loads(response.content.decode())
@@ -75,5 +84,28 @@ if __name__ == "__main__":
       ref = db.reference("line/tokens/" + token_dictionary["key_id"])
       ref.set(token_dictionary["access_token"])
       access_token = token_dictionary["access_token"]
-    
-  print("access_token is " + access_token)
+    return
+
+@app.get("/")
+async def root():
+  return {"message": f"Hello, world"}
+
+@app.post("/webhook", status_code=200)
+async def webhook(
+    request: Request,
+    response: Response,
+    content_length: int = Header(...),
+    x_line_signature: str = Header("x-ling-signature"),
+):
+  logger.log(level=1, msg=request.headers)
+  if(content_length > 1_000_000):
+    response.status_code = 400
+    return {"result": "Content too long"}
+  if(x_line_signature):
+    return response
+
+if __name__ == "__main__":
+
+  reload = True if(mode == "development") else False
+
+  uvicorn.run("main:app", host="127.0.0.1", port=8080, log_level="info", reload=True)
